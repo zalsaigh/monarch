d233d4d6c8fc5af29a5e2459c9fa979c33f97b5b
Michael Le <43327291+LeMichael88@users.noreply.github.com>
2024-11-16 13:44:59 -0500

    Implement Spell Component (#2)
    * Files that godot automatically generated or edited
    
    * Implement spell component, spell class, and sample spells
    
    * Update combat gui to test spells component PoC
    
    * Address comments

--- combat_gui.gd
+++ combat_gui.gd
@@ -1,5 +1,10 @@
 extends Control
 
+signal spell_animation(duration: int)
+
+# TODO: remove this later, just used to test cycling through spells
+var curr_spell: int
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +14,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+# Called when Spells button is pressed (used to test the spells component)
+func _on_spells_button_pressed() -> void:
+	print("Clicked Spell Button")
+	
+	# TODO: get a specific unit's spell component instead of hardcoding player
+	var spells_component = $PlayerContainer/SpellsComponent
+	if spells_component.is_in_group("has_spells"):
+		# Get the next spell (for debugging purposes)
+		var spell_index = curr_spell % spells_component.spells.size()
+		curr_spell += 1
+		
+		# Cast this spell
+		var spell = spells_component.get_spell(spell_index)
+		if spell:
+			spell_animation.emit(spell.animation_time)
+			spell._cast()
