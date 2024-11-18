58972d9f029a31f8a84e576a191c2910112c25b7
Zaid Alsaigh <zalsaigh@uwaterloo.ca>
2024-11-18 01:44:15 -0500

    Update variable names back

--- resources/attack_rsc.gd
+++ resources/attack_rsc.gd
@@ -1,15 +1,15 @@
 class_name Attack extends Resource
 
-@export var m_name : String
-@export var m_description : String
-@export var m_damage : float
-@export_range(0,100) var m_accuracy : float
+@export var name : String
+@export var description : String
+@export var damage : float
+@export_range(0,100) var accuracy : float
 
 @export_category("Sounds")
 @export var attack_execution_sounds : Array[AudioStream] = [] # multiple to loop through
 
-func _init(name: String, description: String, damage=10, accuracy=50):
-	m_name = name
-	m_description = description
-	m_damage = damage
-	m_accuracy = clamp(accuracy, 0, 100) / 100 # percentage division for use in further maths
+func _init(p_name: String, p_description: String, p_damage=10, p_accuracy=50):
+	name = name
+	description = description
+	damage = damage
+	accuracy = clamp(accuracy, 0, 100) / 100 # percentage division for use in further maths
